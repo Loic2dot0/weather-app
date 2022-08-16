c63d0171cca8aef9db3c9ee28ee978262ac23f2c
Loic2dot0 <moreaux.loic@gmail.com>
2022-08-16 17:07:04 +0200

    feat: ajout de la géolocalisation de l'utilisateur Ajout de propriétés pour l'objet API_PARAM création de la fonction de récupération des données de géolocalisation création de la fonction de gestion des erreur de géolocalisation

--- js/app.js
+++ js/app.js
@@ -7,6 +7,39 @@
 const API_PARAM = {
     appid : '', // Your API Key
     lang: 'fr', // langage
-    exclude: 'minutely' //exclude some parts ot the weather data (current,minutely,hourly,daily,alerts)
+    exclude: 'minutely', //exclude some parts ot the weather data (current,minutely,hourly,daily,alerts)
+    lat: 0, // default latitude
+    lon: 0 // default longitude
 }
 
+const ERROR_BOX = document.querySelector(".error-box");
+
+if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(positionSuccess, positionError);
+}
+else{
+    ERROR_BOX.textContent = 'Votre navigateur ne prends pas en charge la géolocalisation.';
+}
+
+function positionSuccess(pos){
+    API_PARAM.lat = pos.coords.latitude;
+    API_PARAM.lon = pos.coords.longitude;
+    console.log(API_PARAM.lat, API_PARAM.lon)
+}
+
+function positionError(err){
+    switch(err.code){
+        case 1:
+            ERROR_BOX.textContent = "Vous n'avez pas autorisé la géolocalisation.";
+            break;
+        case 2:
+            ERROR_BOX.textContent = "Oups... Une erreur est survenue";
+            break;
+        case 3:
+            ERROR_BOX.textContent = "Le délai d'obtention de votre localisation est dépassé.";
+            break;
+        default:
+            ERROR_BOX.textContent = "Oups... Une erreur est survenue";
+            break;
+    }
+}
